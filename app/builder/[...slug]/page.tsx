476439c325313e1ba14e8681ec585e7b6f748f73
Christian Genesis A. Diomampo <chrisgen19@gmail.com>
2025-09-20 16:33:04 +0800

    fix issue with pages /builder/about

--- app/builder/[...slug]/page.tsx
+++ app/builder/[...slug]/page.tsx
@@ -1,15 +1,17 @@
 "use client";
+import { use } from "react";
 import BuilderPage from "../../../src/components/BuilderPage";
 import "../../../src/builder-registry";
 
 interface PageProps {
-  params: {
+  params: Promise<{
     slug: string[];
-  };
+  }>;
 }
 
 export default function DynamicBuilderPage({ params }: PageProps) {
-  const slug = params.slug.join('/');
+  const resolvedParams = use(params);
+  const slug = resolvedParams.slug.join('/');
 
   return <BuilderPage model="page" slug={slug} />;
 }
